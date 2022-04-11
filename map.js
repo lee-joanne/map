54b7a68820de51fdb30cc79505f23367aa3577b8
Joanne <lee.joanne54@gmail.com>
2022-04-11 20:23:37 +0000

    Initial commit

--- map.js
+++ map.js
@@ -1,20 +1,31 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node map.js in the terminal
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5]
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const simplified = nums.map(function (num) { return num * 2 });
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
-
+const exTwo = nums.map(num => num * 2);
+console.log(exTwo)
 
 // With objects:
 const students = [
@@ -37,3 +48,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id])
+console.log(studentsWithIds);
